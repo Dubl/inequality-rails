54a1bd154b8af26dfbbc3c7cf370b0541e9626df
Jon <rmiddlehouse@hotmail.com>
2016-02-21 09:55:49 -0600

    some images converted

---
+++ app/models/contact.rb
@@ -0,0 +1,15 @@
+class Contact < ActiveRecord::Base      
+  attribute :name,       :validate => true
+  attribute :email,      :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+  attribute :attach_file, :attachment => true
+
+  def headers
+    {
+      :subject => "My Form Subject",
+      :to => "useremail@email.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+  
+end
