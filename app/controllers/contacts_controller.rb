3d3783984df3476a1893e3c06ad6073cc41ebc48
Jon <rmiddlehouse@hotmail.com>
2016-02-22 14:54:12 -0600

    email debug1

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,10 +6,12 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.deliver
+    if @contact.deliver     
+      p 'yes'
       redirect_to root_url
       flash[:notice] = 'Thank you for your message. We will contact you soon!'
-    else
+    else    
+      p 'no'
       redirect_to root_url
       flash[:error] = 'Cannot send message.'
     end
