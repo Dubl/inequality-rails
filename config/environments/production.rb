fd2f1a3786082035cb09ce206157636b9dca1639
Jon <rmiddlehouse@hotmail.com>
2016-02-29 19:01:52 -0600

    postmark 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: "smtp.postmarkapp.com",
   port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
+  domain: ENV["POSTMARK_SMTP_SERVER"],
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  user_name: ENV["POSTMARK_API_TOKEN"],
+  password: ENV["POSTMARK_API_TOKEN"]
   }
   # Settings specified here will take precedence over those in config/application.rb.
 
